3a9fe1fdead1aefa5fa1a222f70056bf96b04c0f
Nina Chen <167689128+fluffupie@users.noreply.github.com>
2026-09-09 20:10:44 +1000

    add files via upload

---
+++ C#Tutorial/ObjectOrientedCodeDemo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+public class Account
+{
+    public Account(string bsb, string accountNumber, decimal balance, Person accountHolder)
+    {
+        if (string.IsNullOrWhiteSpace(bsb))
+            throw new ArgumentException("BSB is required.", nameof(bsb));
+
+        if (string.IsNullOrWhiteSpace(accountNumber))
+            throw new ArgumentException("Account number is required.", nameof(accountNumber));
+
+        if (balance < 0)
+            throw new ArgumentOutOfRangeException(nameof(balance), "Balance cannot be negative.");
+
+        AccountHolder = accountHolder ?? throw new ArgumentNullException(nameof(accountHolder));
+
+        Bsb = bsb;
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+
+    public string Bsb { get; }
+    public string AccountNumber { get; }
+    public decimal Balance { get; private set; } // Balance has a private setter, so only the Account class can change it
+    public Person AccountHolder { get; }
+
+    public bool Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Withdrawal amount must be positive.");
+
+        if (amount > Balance)
+            return false;
+
+        Balance -= amount;
+        return true;
+    }
+}
+
+public class Person
+{
+    public Person(string firstName, string lastName, DateTime dateOfBirth)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException("First name is required.", nameof(firstName));
+
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException("Last name is required.", nameof(lastName));
+
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+    }
+
+    public string FirstName { get; }
+    public string LastName { get; }
+    public DateTime DateOfBirth { get; }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
+
+public static class ObjectOrientedCodeDemo
+{
+    public static void Run()
+    {
+        var person = new Person("Bob", "Smith", new DateTime(1990, 6, 20));
+        var account = new Account("062-620", "12341234", 500.00m, person);
+
+        PrintBankDetails(account);
+
+        TryWithdraw(account, 250.00m);
+        TryWithdraw(account, 750.00m);
+        PrintBankDetails(account);
+
+        TryWithdraw(account, 750.00m);
+        PrintBankDetails(account);
+    }
+
+    private static void TryWithdraw(Account account, decimal amount)
+    {
+        if (account.Withdraw(amount))
+        {
+            Console.WriteLine($"Successfully withdrew {amount:C}.");
+        }
+        else
+        {
+            Console.WriteLine($"Withdrawal of {amount:C} failed due to insufficient funds.");
+        }
+    }
+
+    private static void PrintBankDetails(Account account)
+    {
+        Console.WriteLine("Bank Details");
+        Console.WriteLine($"BSB           : {account.Bsb}");
+        Console.WriteLine($"Account Number: {account.AccountNumber}");
+        Console.WriteLine($"Balance       : {account.Balance:C}");
+        Console.WriteLine($"Account Owner : {account.AccountHolder.FullName}");
+        Console.WriteLine();
+    }
+}
---
+++ C#Tutorial/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public struct Point
+{
+    public int XCoord;
+    public int YCoord;
+
+    public Point(int x, int y)
+    {
+        XCoord = x;
+        YCoord = y;
+    }
+
+    public void Print() => Console.WriteLine("x={0}, y={1}", XCoord, YCoord);
+}
+
+public static class PointDemo
+{
+    private static void Swap(Point p)
+    {
+        var x = p.XCoord;
+        p.XCoord = p.YCoord;
+        p.YCoord = x;
+
+        // NOTE: Can also swap via deconstruction.
+        //(p.XCoord, p.YCoord) = (p.YCoord, p.XCoord);
+    }
+
+    public static void Run()
+    {
+        int i = 10; // structs: faster because on stack - i.e., the data is present without additional lookup.
+        string s = ""; // classes: slower because on heap - i.e., a pointer / reference.
+
+        var p = new Point(1, 2);
+        p.Print();
+        Swap(p);
+        p.Print();
+        Console.WriteLine();
+    }
+}
