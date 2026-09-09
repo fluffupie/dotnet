91d2b189d3e50b2c0b15d3fc0113a58721e95c53
Nina Chen <167689128+fluffupie@users.noreply.github.com>
2026-09-09 19:01:02 +1000

    Add Program.cs with Hello World example

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,5 @@
+// Top level statements - C# 9 feature. Default style for modern console projects. The compiler generates 
+// the main method for you
+using System; // imports the System namespace
+
+Console.WriteLine("Hello World!");
