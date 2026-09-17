4523a21acf413c460b5e7e6c93b96dcfb1e788ec
Nina Chen <167689128+fluffupie@users.noreply.github.com>
2026-09-17 12:47:07 +1000

    add files

---
+++ NorthwindWithPagingExample/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace NorthwindWithPagingExample.TagHelpers;
+
+// Version #1.
+[HtmlTargetElement(Attributes = "bold")]
+// Version #2.
+//[HtmlTargetElement("bold")]
+//[HtmlTargetElement(Attributes = "bold")]
+public class BoldTagHelper : TagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        // Version #2.
+        //if(output.TagName == "bold")
+        //{
+        //    output.TagName = "strong";
+        //    return;
+        //}
+
+        output.Attributes.RemoveAll("bold");
+        output.PreContent.SetHtmlContent("<strong>");
+        output.PostContent.SetHtmlContent("</strong>");
+    }
+}
